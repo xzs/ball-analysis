c3e15afb42f9102e611612ccd270cf2749f9d187
Xunzhi Sun <xunzhi.sun@gmail.com>
2015-04-07 23:12:56 -0700

    add by conference, games remain. removed weighted

--- parser.py
+++ parser.py
@@ -38,23 +38,44 @@ TEAMS_DICT = {
     'WAS':'Washington Wizards'
 }
 
-# G   Date                    opponent            Tm  Opp W   L   Streak  Note
-# 0Game Number
-# 1Date
-# 2Time,
-# 3Channel,
-# 4Box Score,
-# 5Home or Away,
-# 6Opponent
-# 7Result,
-# 8OT
-# 9Team Score
-# 10Opponent Score
-# 11Wins
-# 12Loss
-# 13Streak
-# 14Note
-# ['60', 'Sun, Mar 1, 2015', '1:00p EST', 'ABC', 'Box Score', '', 'Los Angeles Clippers', 'L', '', '86', '96', '37', '23', 'L 1', '']
+WESTERN_CONF = {
+    'DAL':'Dallas Mavericks',
+    'DEN':'Denver Nuggets',
+    'GSW':'Golden State Warriors',
+    'HOU':'Houston Rockets',
+    'LAC':'Los Angeles Clippers',
+    'LAL':'Los Angeles Lakers',
+    'MEM':'Memphis Grizzlies',
+    'MIN':'Minnesota Timberwolves',
+    'NOP':'New Orleans Pelicans',
+    'OKC':'Oklahoma City Thunder',
+    'PHO':'Phoenix Suns',
+    'POR':'Portland Trail Blazers',
+    'SAC':'Sacramento Kings',
+    'SAS':'San Antonio Spurs',
+    'UTA':'Utah Jazz'
+}
+
+EASTERN_CONF = {
+    'ATL':'Atlanta Hawks',
+    'BOS':'Boston Celtics',
+    'BRK':'Brooklyn Nets',
+    'CHO':'Charlotte Hornets',
+    'CHI':'Chicago Bulls',
+    'CLE':'Cleveland Cavaliers',
+    'DET':'Detroit Pistons',
+    'IND':'Indiana Pacers',
+    'MIA':'Miami Heat',
+    'MIL':'Milwaukee Bucks',
+    'NYK':'New York Knicks',
+    'ORL':'Orlando Magic',
+    'PHI':'Philadelphia 76ers',
+    'TOR':'Toronto Raptors',
+    'WAS':'Washington Wizards'
+}
+
+def two_decimals(num):
+    return float("{0:.2f}".format(num))
 
 def read_team_schedule_csv():
 
@@ -62,7 +83,7 @@ def read_team_schedule_csv():
     opponents_dict['opp'] = {}
     opponents_dict['channel'] = {}
     
-    with open('team_schedules/CHI.csv', 'rb') as f:
+    with open('team_schedules/MIL.csv', 'rb') as f:
         schedule = csv.reader(f)
         for games in schedule:
             # Count up the number of times the opponent is supposed to be played this season
@@ -89,41 +110,6 @@ def read_team_schedule_csv():
 
     return opponents_dict
 
-def two_decimals(num):
-    return float("{0:.2f}".format(num))
-# 1,1,2014-10-29,23-078,MIL,@,CHO,L (-2),1,35:56,7,15,.467,0,2,.000,3,5,.600,1,5,6,2,0,1,4,2,17,8.0,-5
-'''
-0Rk
-1Game played
-2Date
-3Age
-4team
-5Away
-6opponent
-7Result
-8Game started
-9Minutes played
-10FGM
-11FGA
-12FG%
-133PM
-143PA
-153P%
-16FTM
-17FTA
-18FT%
-19ORD
-20DRB
-21TRB
-22AST
-23STL
-24BLK
-25TOV
-26PF
-27PTS
-28GmSc
-29+/-
-'''
 
 '''
 GmSc
@@ -137,22 +123,26 @@ def read_player_csv(schedule):
     with open('player_logs/Khris Middleton.csv', 'rb') as f:
         player_log = csv.reader(f)
 
-        # pp.pprint(schedule)
-
         player_dict = {}
         player_dict['teams_against'] = {}
+        player_dict['eastern_conf'] = {}
+        east_gmsc = 0
+        player_dict['eastern_conf']['games'] = 0
+        player_dict['western_conf'] = {}
+        west_gmsc = 0
+        player_dict['western_conf']['games'] = 0
         away_games = 0
         home_games = 0
         away_gmsc = 0
         home_gmsc = 0
         home_playtime_seconds = 0
         away_playtime_seconds = 0
+
         # We want to be able to create a player dictionary that will contain the statistics for the GmSc.
         # The dictionary will also contain detailed information abou the teams the player has played agianst
         for record in player_log:
             # If he played
             if record[1]:
-
                 if record[5]:
                     away_gmsc += float(record[28])
                     away_games += 1
@@ -172,6 +162,15 @@ def read_player_csv(schedule):
                     player_dict['teams_against'][record[6]]['games'] = 1
                     player_dict['teams_against'][record[6]]['gmsc'] = float(record[28])
 
+                player_dict['teams_against'][record[6]]['games_remain'] = schedule['opp'][TEAMS_DICT[record[6]]] - player_dict['teams_against'][record[6]]['games']
+
+                if record[6] in EASTERN_CONF:
+                    player_dict['eastern_conf']['games'] += 1
+                    east_gmsc += float(record[28])
+                else:
+                    player_dict['western_conf']['games'] += 1
+                    west_gmsc += float(record[28])
+
 
         player_dict['home_playtime'] = two_decimals(float(home_playtime_seconds / away_games)/60)
         player_dict['away_playtime'] = two_decimals(float(away_playtime_seconds / home_games)/60)
@@ -180,19 +179,13 @@ def read_player_csv(schedule):
         player_dict['average_home_gmsc'] = two_decimals(float(home_gmsc / home_games))
         player_dict['average_gmsc'] = two_decimals(float((away_gmsc+home_gmsc)/(away_games+home_games)))
 
-        # calc_weighted_average(schedule, player_dict)
+        player_dict['eastern_conf']['gmsc'] = two_decimals(float(east_gmsc / player_dict['eastern_conf']['games']))
+        player_dict['western_conf']['gmsc'] = two_decimals(float(west_gmsc / player_dict['western_conf']['games']))
+
         player_dict['cov'] = calc_coefficient_of_variance(player_dict)
 
     pp.pprint(player_dict)
     return player_dict
-
-def calc_weighted_average(schedule, player_dict):
-
-    opponents = schedule['opp']
-    for team in player_dict['teams_against']:
-        if TEAMS_DICT[team] in opponents:
-            print opponents[TEAMS_DICT[team]]
-
 
 def calc_coefficient_of_variance(player_dict):
 
