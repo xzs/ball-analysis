d00ae848526b162ab78f1414512f8109684f2410
Xunzhi Sun <xunzhi.sun@gmail.com>
2015-10-25 11:48:50 -0700

    adding angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ballAnalysisApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ballAnalysisApp
+ */
+angular.module('ballAnalysisApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ballAnalysisApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ballAnalysisApp
+ */
+angular.module('ballAnalysisApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
