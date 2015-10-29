65486d31985bf4a9ee5a71549a09b4dd46d5c156
Xunzhi Sun <xunzhi.sun@gmail.com>
2015-10-28 23:59:09 -0700

    moving files, reading json, tables

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,8 +16,8 @@ app.controller('MainCtrl',
     var salary = 50000;
 
     function fetchCSV() {
-        // get the csv
         fetch.getCSV().then(function (response) {
+            // use papa to parse the csv to json
             Papa.parse(response, {
                 complete: function(results) {
                     // remove the first element from the list
@@ -27,6 +27,17 @@ app.controller('MainCtrl',
         })
     };
 
+    function fetchPlayer(name) {
+        $scope.playerName = name;
+        fetch.getPlayer(name).then(function (response) {
+            // console.log();
+            var playerTeamData = response.teams_against;
+            $scope.teamDataHeader = Object.keys(playerTeamData);
+            $scope.teamData = playerTeamData;
+            // against teams
+        })
+    }
+
     function processCSV(data) {
         // process the data into readable JSON format
         processing.setAllPlayersByPosition(data);
@@ -35,5 +46,6 @@ app.controller('MainCtrl',
     };
 
     fetchCSV();
+    fetchPlayer('Aaron Gordon');
 
 }]);
--- app/scripts/services/fetch.js
+++ app/scripts/services/fetch.js
@@ -1,7 +1,12 @@
 app.factory('fetch', ['$http', function($http) {
     return {
         getCSV: function() {
-            return $http.get('/csv/DKSalaries.csv').then(function (response) {
+            return $http.get('csv/DKSalaries.csv').then(function (response) {
+                return response.data;
+            });
+        },
+        getPlayer: function(name) {
+            return $http.get('scrape/json_files/'+name+'.json').then(function (response) {
                 return response.data;
             });
         }
