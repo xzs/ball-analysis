20570532907d257e704f5ca49ac29d8ee039cfa7
Xunzhi Sun <xunzhi.sun@gmail.com>
2015-12-13 22:40:20 -0800

    refactor code for parser

--- app/scrape/parser.py
+++ app/scrape/parser.py
@@ -248,21 +248,39 @@ def read_player_csv(csv_f, schedule, player_name):
     turnovers = 0
     threes = 0
 
-    started_points = 0
-    started_rebounds = 0
-    started_assists = 0
-    started_steals = 0
-    started_blocks = 0
-    started_turnovers = 0
-    started_threes = 0
+    stats_dict = {
+        'all': {
+            'points' : 0,
+            'rebounds' : 0,
+            'assists' : 0,
+            'steals' : 0,
+            'blocks' : 0,
+            'turnovers' : 0,
+            'threes' : 0,
+            'games': 0
+        },
+        'as_starter': {
+            'points' : 0,
+            'rebounds' : 0,
+            'assists' : 0,
+            'steals' : 0,
+            'blocks' : 0,
+            'turnovers' : 0,
+            'threes' : 0,
+            'games': 0
+        },
+        'non_starter': {
+            'points' : 0,
+            'rebounds' : 0,
+            'assists' : 0,
+            'steals' : 0,
+            'blocks' : 0,
+            'turnovers' : 0,
+            'threes' : 0,
+            'games': 0
+        }
 
-    non_started_points = 0
-    non_started_rebounds = 0
-    non_started_assists = 0
-    non_started_steals = 0
-    non_started_blocks = 0
-    non_started_turnovers = 0
-    non_started_threes = 0
+    }
 
     east_gmsc = 0
     west_gmsc = 0
@@ -301,24 +319,24 @@ def read_player_csv(csv_f, schedule, player_name):
         if record[1]:
 
             if record[5]:
-                logger.info('Compling away games')
+                logger.debug('Compling away games')
                 away_gmsc += float(record[28])
                 away_games += 1
                 away_playtime_seconds = process_playtime(away_playtime_seconds, record[9])
             else:
-                logger.info('Compling home games')
+                logger.debug('Compling home games')
                 home_gmsc += float(record[28])
                 home_games += 1
                 home_playtime_seconds = process_playtime(home_playtime_seconds, record[9])
 
             # for games the player started
             if record[8] == '1':
-                logger.info('Compling home games')
+                logger.debug('Compling starter games')
                 started_gmsc += float(record[28])
                 started_games += 1
                 started_playtime_seconds = process_playtime(started_playtime_seconds, record[9])
             else:
-                logger.info('Compling home games')
+                logger.debug('Compling non_starter games')
                 non_started_gmsc += float(record[28])
                 non_started_games += 1
                 non_started_playtime_seconds = process_playtime(non_started_playtime_seconds, record[9])
@@ -326,7 +344,7 @@ def read_player_csv(csv_f, schedule, player_name):
             # calculate the playtime for the player
             play_time_seconds = process_playtime(play_time_seconds, record[9])
 
-            logger.info('Begining game statistics')
+            logger.debug('Begining game statistics')
             # Create new layers for statistics against every team
             new_stats_dict(player_dict['teams_against'], record[6], record)
 
@@ -337,7 +355,7 @@ def read_player_csv(csv_f, schedule, player_name):
             assists_list.append(float(record[22]))
             rebounds_list.append(float(record[21]))
 
-            logger.info('Compling conference play')
+            logger.debug('Compling conference play')
             if record[6] in EASTERN_CONF:
                 player_dict['eastern_conf']['games'] += 1
                 east_gmsc += float(record[28])
@@ -345,31 +363,12 @@ def read_player_csv(csv_f, schedule, player_name):
                 player_dict['western_conf']['games'] += 1
                 west_gmsc += float(record[28])
 
-
             if record[8] == '1':
-                started_points += float(record[27])
-                started_rebounds += float(record[21])
-                started_assists += float(record[22])
-                started_steals += float(record[23])
-                started_blocks += float(record[24])
-                started_turnovers += float(record[25])
-                started_threes += float(record[13])
+                stats_dict['as_starter'] = process_basic_stats(stats_dict['as_starter'], record)
             else:
-                non_started_points += float(record[27])
-                non_started_rebounds += float(record[21])
-                non_started_assists += float(record[22])
-                non_started_steals += float(record[23])
-                non_started_blocks += float(record[24])
-                non_started_turnovers += float(record[25])
-                non_started_threes += float(record[13])
+                stats_dict['non_starter'] = process_basic_stats(stats_dict['non_starter'], record)
 
-            points += float(record[27])
-            rebounds += float(record[21])
-            assists += float(record[22])
-            steals += float(record[23])
-            blocks += float(record[24])
-            turnovers += float(record[25])
-            threes += float(record[13])
+            stats_dict['all'] = process_basic_stats(stats_dict['all'], record)
 
             # If any games the player played before the ALL_STAR_BREAK
             all_star = dt.strptime(ALL_STAR_DATE, '%Y-%m-%d')
@@ -387,8 +386,8 @@ def read_player_csv(csv_f, schedule, player_name):
 
     #  For now we only consider players who have played both a home and away game
     if home_games > 0 or away_games > 0:
-        logger.info('First level dictionary values processing')
-        # value_when_true if condition else value_when_false
+        logger.debug('First level dictionary values processing')
+        # calc home and away playtime + gmsc
         if home_games > 0:
             player_dict['home_playtime'] = two_decimals(float(home_playtime_seconds / home_games)/60)
             player_dict['average_home_gmsc'] = two_decimals(float(home_gmsc / home_games))
@@ -403,6 +402,7 @@ def read_player_csv(csv_f, schedule, player_name):
             player_dict['away_playtime'] = 0
             player_dict['average_away_gmsc'] = 0
 
+        # calc starter and non starter playtime + gmsc
         if started_games > 0:
             player_dict['started_playtime'] = two_decimals(float(started_playtime_seconds / started_games)/60)
             player_dict['average_started_gmsc'] = two_decimals(float(started_gmsc / started_games))
@@ -417,11 +417,7 @@ def read_player_csv(csv_f, schedule, player_name):
             player_dict['non_started_playtime'] = 0
             player_dict['average_non_started_gmsc'] = 0
 
-        player_dict['stats']['playtime'] = two_decimals(float(play_time_seconds / (away_games + home_games))/60)
-
-        player_dict['as_starter']['playtime'] = player_dict['started_playtime']
-        player_dict['non_starter']['playtime'] = player_dict['non_started_playtime']
-
+        # calc conferences gmsc
         if player_dict['eastern_conf']['games'] != 0:
             player_dict['eastern_conf']['gmsc'] = two_decimals(float(east_gmsc / player_dict['eastern_conf']['games']))
         else:
@@ -432,43 +428,24 @@ def read_player_csv(csv_f, schedule, player_name):
         else:
             player_dict['western_conf']['gmsc'] = 0
 
+        # set playtimes
+        player_dict['stats']['playtime'] = two_decimals(float(play_time_seconds / (away_games + home_games))/60)
+        player_dict['as_starter']['playtime'] = player_dict['started_playtime']
+        player_dict['non_starter']['playtime'] = player_dict['non_started_playtime']
 
         if started_games > 0:
-            player_dict['as_starter']['points'] = two_decimals(float(started_points / started_games))
-            player_dict['as_starter']['rebounds'] = two_decimals(float(started_rebounds / started_games))
-            player_dict['as_starter']['assists'] = two_decimals(float(started_assists / started_games))
-            player_dict['as_starter']['steals'] = two_decimals(float(started_steals / started_games))
-            player_dict['as_starter']['blocks'] = two_decimals(float(started_blocks / started_games))
-            player_dict['as_starter']['turnovers'] = two_decimals(float(started_turnovers / started_games))
-            player_dict['as_starter']['threes'] = two_decimals(float(started_threes / started_games))
-            # avg gmscr
-            player_dict['as_starter']['gmsc'] = two_decimals(float(player_dict['average_started_gmsc']/started_games))
-            player_dict['as_starter']['dk_points'] = calc_dk_points(player_dict['as_starter'])
+            stats_dict['as_starter']['games'] = started_games
+            player_dict['as_starter'] = process_summary_stats_def(player_dict['as_starter'], stats_dict['as_starter'], player_dict['average_started_gmsc'])
 
         if non_started_games > 0:
-            player_dict['non_starter']['points'] = two_decimals(float(non_started_points / non_started_games))
-            player_dict['non_starter']['rebounds'] = two_decimals(float(non_started_rebounds / non_started_games))
-            player_dict['non_starter']['assists'] = two_decimals(float(non_started_assists / non_started_games))
-            player_dict['non_starter']['steals'] = two_decimals(float(non_started_steals / non_started_games))
-            player_dict['non_starter']['blocks'] = two_decimals(float(non_started_blocks / non_started_games))
-            player_dict['non_starter']['turnovers'] = two_decimals(float(non_started_turnovers / non_started_games))
-            player_dict['non_starter']['threes'] = two_decimals(float(non_started_threes / non_started_games))
-            # avg gmscr
-            player_dict['non_starter']['gmsc'] = two_decimals(float(player_dict['average_non_started_gmsc']/non_started_games))
-            player_dict['non_starter']['dk_points'] = calc_dk_points(player_dict['non_starter'])
+            stats_dict['non_starter']['games'] = non_started_games
+            player_dict['non_starter'] = process_summary_stats_def(player_dict['non_starter'], stats_dict['non_starter'], player_dict['average_non_started_gmsc'])
 
 
-        player_dict['stats']['points'] = two_decimals(float(points / (away_games + home_games)))
-        player_dict['stats']['rebounds'] = two_decimals(float(rebounds / (away_games + home_games)))
-        player_dict['stats']['assists'] = two_decimals(float(assists / (away_games + home_games)))
-        player_dict['stats']['steals'] = two_decimals(float(steals / (away_games + home_games)))
-        player_dict['stats']['blocks'] = two_decimals(float(blocks / (away_games + home_games)))
-        player_dict['stats']['turnovers'] = two_decimals(float(turnovers / (away_games + home_games)))
-        player_dict['stats']['threes'] = two_decimals(float(threes / (away_games + home_games)))
-        # avg gmscr
-        player_dict['stats']['gmsc'] = two_decimals(float((away_gmsc + home_gmsc)/(away_games + home_games)))
-        player_dict['stats']['dk_points'] = calc_dk_points(player_dict['stats'])
+        stats_dict['all']['games'] = away_games + home_games
+        player_dict['stats'] = process_summary_stats_def(player_dict['stats'], stats_dict['all'], away_gmsc + home_gmsc)
 
+        # variance of data
         player_dict['cov'] = calc_coefficient_of_variance(player_dict)
 
         player_dict['best_stretch'] = {}
@@ -484,6 +461,32 @@ def read_player_csv(csv_f, schedule, player_name):
 
     return player_dict
 
+def process_basic_stats(dict_obj, record):
+    dict_obj['points'] += float(record[27])
+    dict_obj['rebounds'] += float(record[21])
+    dict_obj['assists'] += float(record[22])
+    dict_obj['steals'] += float(record[23])
+    dict_obj['blocks'] += float(record[24])
+    dict_obj['turnovers'] += float(record[25])
+    dict_obj['threes'] += float(record[13])
+
+    return dict_obj
+
+
+def process_summary_stats_def(dict_obj, stats_dict, gmsc):
+
+    dict_obj['points'] = two_decimals(float(stats_dict['points'] / stats_dict['games']))
+    dict_obj['rebounds'] = two_decimals(float(stats_dict['rebounds'] / stats_dict['games']))
+    dict_obj['assists'] = two_decimals(float(stats_dict['assists'] / stats_dict['games']))
+    dict_obj['steals'] = two_decimals(float(stats_dict['steals'] / stats_dict['games']))
+    dict_obj['blocks'] = two_decimals(float(stats_dict['blocks'] / stats_dict['games']))
+    dict_obj['turnovers'] = two_decimals(float(stats_dict['turnovers'] / stats_dict['games']))
+    dict_obj['threes'] = two_decimals(float(stats_dict['threes'] / stats_dict['games']))
+    # avg gmscr
+    dict_obj['gmsc'] = two_decimals(float(gmsc /stats_dict['games']))
+    dict_obj['dk_points'] = calc_dk_points(dict_obj)
+
+    return dict_obj
 
 def process_playtime(playtime_seconds, record):
     playtime = record.split(':')
@@ -602,7 +605,7 @@ The other thing is that I want the largest consecutive sum given a period
 if n = 3, then the largest sum in this case would be (4+5+7) = 16
 '''
 def consecutive_sum(stats_list, n):
-    logger.info('Process consecutive sum')
+    logger.debug('Process consecutive sum')
     max_sum = 0
     max_hash = 0
     #  we use enumerate to access the index
@@ -625,7 +628,7 @@ def consecutive_sum(stats_list, n):
 
 
 def calc_coefficient_of_variance(player_dict):
-    logger.info('COV processing')
+    logger.debug('COV processing')
     total = 0
     num_teams = len(player_dict['teams_against'])
     for team in player_dict['teams_against']:
@@ -653,7 +656,7 @@ def calc_coefficient_of_variance(player_dict):
 def last_n_games(csv_f, num_games):
     # We read the file backwards from the csv file
     # However, if the file does not fit in memory this method of using reverse will not work
-    logger.info('Best stretch processing')
+    logger.debug('Best stretch processing')
     count = 0
     PLAYER_DICT['last_'+str(num_games)+'_games'] = {}
     playtime = 0
@@ -709,9 +712,7 @@ SCHEDULE_DICT = {}
 SCHEDULE_DICT['league_schedule'] = {}
 for files in glob.glob('team_schedules/'+YEAR+'/*.csv'):
     team_name = files.split('/')[2].split('.c')[0]
-    logger.info('Parsing schedule for: '+team_name)
 
-    # this will need to be looped
     with open(files, 'rb') as f:
         try:
             read_team_schedule_csv(f, team_name)
@@ -729,9 +730,6 @@ ALL_PLAYERS = {}
 # Open all player files for data parsing
 for files in glob.glob('player_logs/'+YEAR+'/*.csv'):
     player_name = files.split('/')[2].split('.c')[0]
-
-    # this will need to be looped
-    logger.info('Parsing stats for: '+player_name)
 
     with open(files, 'rb') as f:
         try:
